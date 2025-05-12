502d0e32d5891dbe1d5bea73383e0fbc194b7517
kogora <it.is.mr.lag@gmail.com>
2025-05-12 17:02:22 +0300

    Update test logger cfg

--- giraph/src/main/java/org/myexample/Fun.java
+++ giraph/src/main/java/org/myexample/Fun.java
@@ -12,20 +12,26 @@ public class Fun {
 
     public static void main(String[] args) throws Exception {
 
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
         GiraphConfiguration conf = new GiraphConfiguration();
         GiraphRunner runner = new GiraphRunner();
+
+        // 1. Configure logging directory
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+        String timestamp = System.getProperty("giraph.job.timestamp");
+        String outputPath = System.getProperty("giraph.log.dir");
+
         runner.setConf(conf);
         System.exit(ToolRunner.run(runner, new String[]{
                 GiraphShortestDistance.class.getName(),
                 "-vip", "src/main/resources/giraph/tiny_graph.txt",
                 "-vif", "org.apache.giraph.io.formats.JsonLongDoubleFloatDoubleVertexInputFormat",
                 "-vof", "org.apache.giraph.io.formats.IdWithValueTextOutputFormat",
-                "-op", "src/main/resources/giraph/output/" + dateFormat.format(new Date()),
+                "-op", outputPath + "/res",
                 "-w", "1",
                 "-ca", "mapred.job.tracker=local",
                 "-ca", "giraph.SplitMasterWorker=false",
-                "-ca", "giraph.useSuperstepCounters=false"}));
+                "-ca", "giraph.useSuperstepCounters=false",
+                "-ca", "mapreduce.joboutput.outputformat.overwrite=true"}));
         /* Two other useful properties when writing complex algorithms */
         //"-ca", "giraph.masterComputeClass=YourMasterClass",
         //"-ca", "giraph.workerContextClass=YourWorkerContextClass",
