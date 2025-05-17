7c53f6f778a8202d1ff23a86dd293b4abd9781be
kogora <it.is.mr.lag@gmail.com>
2025-05-18 00:12:56 +0300

    Implement basic example

--- giraph/src/main/java/org/myexample/Fun.java
+++ giraph/src/main/java/org/myexample/Fun.java
@@ -1,39 +1,20 @@
 package org.myexample;
 
-import org.apache.giraph.GiraphRunner;
-import org.apache.giraph.conf.GiraphConfiguration;
-import org.apache.hadoop.util.ToolRunner;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import org.CommonRunner;
+import org.apache.giraph.io.formats.IdWithValueTextOutputFormat;
+import org.apache.giraph.io.formats.JsonLongDoubleFloatDoubleVertexInputFormat;
 
 public class Fun {
 
-    public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
 
-        GiraphConfiguration conf = new GiraphConfiguration();
-        GiraphRunner runner = new GiraphRunner();
-
-        // 1. Configure logging directory
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
-        String timestamp = System.getProperty("giraph.job.timestamp");
-        String outputPath = System.getProperty("giraph.log.dir");
-
-        runner.setConf(conf);
-        System.exit(ToolRunner.run(runner, new String[]{
-                GiraphShortestDistance.class.getName(),
-                "-vip", "src/main/resources/giraph/tiny_graph.txt",
-                "-vif", "org.apache.giraph.io.formats.JsonLongDoubleFloatDoubleVertexInputFormat",
-                "-vof", "org.apache.giraph.io.formats.IdWithValueTextOutputFormat",
-                "-op", outputPath + "/res",
-                "-w", "1",
-                "-ca", "mapred.job.tracker=local",
-                "-ca", "giraph.SplitMasterWorker=false",
-                "-ca", "giraph.useSuperstepCounters=false",
-                "-ca", "mapreduce.joboutput.outputformat.overwrite=true"}));
-        /* Two other useful properties when writing complex algorithms */
-        //"-ca", "giraph.masterComputeClass=YourMasterClass",
-        //"-ca", "giraph.workerContextClass=YourWorkerContextClass",
-    }
+    System.exit(
+        CommonRunner.run(
+            GiraphShortestDistance.class.getName(),
+            new String[] {
+              "-vip", System.getProperty("giraph.input.graph"),
+              "-vif", JsonLongDoubleFloatDoubleVertexInputFormat.class.getName(),
+              "-vof", IdWithValueTextOutputFormat.class.getName(),
+            }));
+  }
 }
