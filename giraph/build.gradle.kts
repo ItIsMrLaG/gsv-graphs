0b5633ab3f0b8e9575ee9b1778a8ee37e53bca78
kogora <it.is.mr.lag@gmail.com>
2025-05-18 00:15:10 +0300

    Commit misc

--- giraph/build.gradle.kts
+++ giraph/build.gradle.kts
@@ -1,15 +1,9 @@
-import org.gradle.internal.classpath.Instrumented.systemProperty
-import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.Date
 
 plugins {
     id("java")
-    id("application")
-}
-
-application {
-    mainClass = "org.myexample.Fun"
+    id("com.diffplug.spotless") version "6.13.0"
 }
 
 group = "org.example"
@@ -21,29 +15,82 @@ repositories {
 }
 
 dependencies {
+    implementation("org.apache.hadoop:hadoop-client:2.7.7")
     implementation("org.apache.giraph:giraph-core:1.3.0-SNAPSHOT") {
         isChanging = true
     }
-
-    implementation("org.apache.hadoop:hadoop-client:2.7.7")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
-tasks.named<JavaExec>("run") {
-    val dateFormat: DateFormat = SimpleDateFormat("yyyy-MM-dd-HH-mm-ss")
-    val timestamp = dateFormat.format(Date())
-    val outputPath = "src/main/resources/giraph/output/" + timestamp
-
-    systemProperty("giraph.log.dir", outputPath)
-    systemProperty("giraph.job.timestamp", timestamp)
-
-    jvmArgs = listOf(
-        "-Dlog4j.configuration=file:${project.buildDir}/resources/main/log4j.properties"
-    )
-}
-
 tasks.test {
     useJUnitPlatform()
 }
+
+spotless {
+    java {
+        target("src/**/*.java") // Указываем, какие файлы проверять
+        googleJavaFormat("1.7") // Или другую версию / формат по вкусу
+    }
+}
+
+val GIRAPH_DIR = File("src/main/resources/giraph/")
+val GIRAPH_RUNCFG_DIR = File("$GIRAPH_DIR/runcfg/")
+val GIRAPH_OUTPUT_DIR = File("$GIRAPH_DIR/output/")
+
+/* ============== EXAMPLE ==============   */
+
+tasks.register<JavaExec>("runExample") {
+    dependsOn(exampleJar)
+    classpath = files(exampleJar.get().archiveFile) +
+               configurations.runtimeClasspath.get()
+
+    group = "Execution"
+    mainClass.set("org.myexample.Fun")
+    systemProperty("giraph.output.dir", "$GIRAPH_OUTPUT_DIR/example/${getTimestamp()}")
+}
+
+/* ============== MS-BFS EXAMPLE ==============   */
+
+val MSBFS_NAME = "msbfs"
+
+tasks.register<JavaExec>("runMSBfsExample") {
+    dependsOn(exampleJar)
+    classpath = files(exampleJar.get().archiveFile) +
+               configurations.runtimeClasspath.get()
+    mainClass.set("org.algos.$MSBFS_NAME.runexample.Fun")
+
+    group = "Execution"
+    systemProperty("giraph.output.dir", "$GIRAPH_OUTPUT_DIR/$MSBFS_NAME/${getTimestamp()}")
+    systemProperty("giraph.input.graph", "$GIRAPH_RUNCFG_DIR/$MSBFS_NAME/example_graph.txt")
+    systemProperty("giraph.input.sourceIds", "$GIRAPH_RUNCFG_DIR/$MSBFS_NAME/source_ids.txt")
+}
+
+/* ============== BORUVKA EXAMPLE ==============   */
+
+val BORUVKA_NAME = "boruvka"
+
+tasks.register<JavaExec>("runBoruvkaExample") {
+    dependsOn(exampleJar)
+    classpath = files(exampleJar.get().archiveFile) +
+               configurations.runtimeClasspath.get()
+    mainClass.set("org.algos.$BORUVKA_NAME.runexample.Fun")
+
+    group = "Execution"
+    systemProperty("giraph.output.dir", "$GIRAPH_OUTPUT_DIR/$BORUVKA_NAME/${getTimestamp()}")
+    systemProperty("giraph.input.graph", "$GIRAPH_RUNCFG_DIR/$BORUVKA_NAME/example_graph.txt")
+
+//    jvmArgs = listOf("-Xmx512m")
+}
+
+/* ============== TOOLS ==============   */
+
+val exampleJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("example")
+    from(sourceSets.main.get().output)
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
+fun getTimestamp() = SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(Date())
